56af0e2f1b6b833289d5d3fc6723c94ac1c2606a
Mert Gül <gul.mert89@gmail.com>
2021-01-06 19:00:28 +0300

    7

--- video_capture_streamlit.py
+++ video_capture_streamlit.py
@@ -24,61 +24,13 @@ if __name__ == "__main__":
     count = 0
     if st_button("Click here to open the camera."):
         st_text("(No worries. The camera will be automatically closed.)")
-        """while count < 100:
+        while count < 100:
             ret, frame = cap.read()
             if not ret:
                 print("Can't receive the frame.")
                 break
-            
-            faces, confidences = detect_face(frame, threshold=detectFace_threshold)
-            for f in faces: 
-                (startX, startY) = (f[0], f[1])
-                (endX, endY) = (f[2], f[3])
-                cropped_frame = copy(frame[startY:endY, startX:endX])
-                if (cropped_frame.shape[0]) < 10 or (cropped_frame.shape[1]) < 10:
-                    continue
-
-                cropped_frame = resize(cropped_frame, (64,64))        
-                cropped_frame = cvtColor(cropped_frame, code=6)
-                cropped_frame = cropped_frame.astype("float32") / 255.0
-                cropped_frame = expand_dims(cropped_frame, axis=[0,3])
-                confidences = model.predict(cropped_frame)[0]
-                max_probability = max(confidences)*100
-                classIndex = argmax(confidences)
-                className = getClassName(classIndex)
-
-                if max_probability > predictFace_threshold:
-                    frame_text = f"{className} ({int(max_probability)}%)"
-                    if className == "Shocked":
-                        rect_color = text_color = (0, 255, 255)
-                    elif className == "Sad":
-                        rect_color = text_color = (0, 0, 255)
-                    elif className == "Happy":
-                        rect_color = text_color = (0, 255, 0)
-                    elif className == "Poker Face":
-                        rect_color = text_color = (214, 112, 218)
-                else:
-                    rect_color = text_color = (255, 255, 0)
-                    frame_text = "Reading..."    
-
-                rectangle(img=frame, 
-                        pt1=(startX, startY), 
-                        pt2=(endX, endY), 
-                        color=rect_color, 
-                        thickness=2)
-
-                startY = startY - 10 if startY - 10 > 10 else startY + 10
-                putText(img=frame, 
-                        text=frame_text, 
-                        org=(startX, startY), 
-                        fontFace=FONT_HERSHEY_COMPLEX, 
-                        lineType=LINE_AA,
-                        fontScale=0.6, 
-                        color=text_color, 
-                        thickness=1)
-            #frame = cvtColor(frame, code=2)    # works slower! Use frame[..., ::-1]
             WINDOW.image(frame[..., ::-1])
             count += 1
         cap.release()
-        destroyAllWindows()"""
+        destroyAllWindows()
         st_text("THANKS FOR GIVING MY MODEL A SHOT! :)")
