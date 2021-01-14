bee6f1a790702163776042cedeb473f613d78645
Mert Gül <gul.mert89@gmail.com>
2021-01-14 12:55:46 +0300

    Delete setup.sh

--- setup.sh
+++
@@ -1,9 +0,0 @@
-mkdir -p ~/.streamlit/
-echo "[general]
-email = \"gul.mert89@gmail.com\"
-" > ~/.streamlit/credentials.toml
-echo "[server]
-headless = true
-port = $PORT
-enableCORS = false
-" > ~/.streamlit/config.toml
