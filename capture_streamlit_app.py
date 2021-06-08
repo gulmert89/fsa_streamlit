2086d7531ffe9d039e6a43c6df0bac630a99f581
Mert Gül <gul.mert89@gmail.com>
2021-06-08 10:12:53 +0300

    Change name of the deploy file

--- capture_streamlit_app.py
+++ capture_streamlit_app.py
@@ -11,14 +11,16 @@ model = load_model(r'./model/')
 
 st_title("Facial Sentiment Analysis")
 
+
 def getClassName(classIndex):
     "A basic function to get the class name."
-    if classIndex==3:   return "Happy"    
-    elif classIndex==1: return "Sad"    
-    elif classIndex==2: return "Shocked"
-    else:               return "Poker Face"
+    if classIndex == 3:   return "Happy"
+    elif classIndex == 1: return "Sad"
+    elif classIndex == 2: return "Shocked"
+    else:                 return "Poker Face"
 
-if __name__ == "__main__":    
+
+if __name__ == "__main__":
     detectFace_threshold = 0.70
     predictFace_threshold = 0.35 * 100
     image_file = st_file_uploader("Upload your selfie here:", type=["jpg", "jpeg", "png"])
@@ -27,9 +29,9 @@ if __name__ == "__main__":
         image_file = Image.open(image_file)
         try:
             for orientation in ExifTags.TAGS.keys():
-                if ExifTags.TAGS[orientation]=='Orientation':
+                if ExifTags.TAGS[orientation] == 'Orientation':
                     exif_orientation = orientation
-                    break        
+                    break
             exif = image_file._getexif()
             if exif[exif_orientation] == 3:
                 image_file = image_file.rotate(180, expand=True)
@@ -37,7 +39,7 @@ if __name__ == "__main__":
                 image_file = image_file.rotate(270, expand=True)
             elif exif[exif_orientation] == 8:
                 image_file = image_file.rotate(90, expand=True)
-        except:
+        except Exception:
             pass
         frame = array(image_file)        
         height = 480
@@ -45,23 +47,24 @@ if __name__ == "__main__":
         frame = resize(frame, (height, width))
         if frame.shape[2] == 4:    # alpha channel will be removed
             frame = cvtColor(frame, code=1)    # BGRA2BGR
-        else: pass 
+        else:
+            pass
         faces, confidences = detect_face(frame, threshold=detectFace_threshold)
         for f in faces:
-            # corner points of facial frame: 
+            # corner points of facial frame:
             (startX, startY) = (f[0], f[1])    # top left corner
             (endX, endY) = (f[2], f[3])    # bottom right corner
             # crop it from the whole frame:
-            cropped_frame = copy(frame[startY:endY, startX:endX])        
+            cropped_frame = copy(frame[startY:endY, startX:endX])
             # skip too small frames (10x10 pixels)
             if (cropped_frame.shape[0]) < 10 or (cropped_frame.shape[1]) < 10:
                 continue
             # preprocessing on the cropped frame
-            cropped_frame = resize(cropped_frame, (64,64))        
+            cropped_frame = resize(cropped_frame, (64, 64))
             cropped_frame = cvtColor(cropped_frame, code=6)    # BGR2GRAY
             cropped_frame = cropped_frame.astype("float32") / 255.0
-            cropped_frame = expand_dims(cropped_frame, axis=[0,3])
-            confidences = model.predict(cropped_frame)[0]    # probability value
+            cropped_frame = expand_dims(cropped_frame, axis=[0, 3])
+            confidences = model.predict(cropped_frame)[0]  # probability value
             max_probability = max(confidences)*100
             classIndex = argmax(confidences)
             className = getClassName(classIndex)
@@ -80,23 +83,23 @@ if __name__ == "__main__":
 
             else:
                 rect_color = text_color = (255, 0, 0)
-                frame_text = "Reading..."    
+                frame_text = "Reading..."
 
-            rectangle(img=frame, 
-                        pt1=(startX, startY), 
-                        pt2=(endX, endY), 
-                        color=rect_color, 
-                        thickness=2)
+            rectangle(img=frame,
+                      pt1=(startX, startY),
+                      pt2=(endX, endY),
+                      color=rect_color,
+                      thickness=2)
 
             # let's keep the text in the frame and avoid edges:
             startY = startY - 10 if startY - 10 > 10 else startY + 10
-            putText(img=frame, 
-                        text=frame_text, 
-                        org=(startX, startY), 
-                        fontFace=FONT_HERSHEY_COMPLEX, 
-                        lineType=LINE_AA,
-                        fontScale=0.6, 
-                        color=text_color, 
-                        thickness=1)
+            putText(img=frame,
+                    text=frame_text,
+                    org=(startX, startY),
+                    fontFace=FONT_HERSHEY_COMPLEX,
+                    lineType=LINE_AA,
+                    fontScale=0.6,
+                    color=text_color,
+                    thickness=1)
 
         st_image(frame)
